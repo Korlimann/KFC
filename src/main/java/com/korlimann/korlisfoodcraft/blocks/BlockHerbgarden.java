af13b6ac661cef807692dabdd64878af3f75be04
Stefan Kormann <kor17541@spengergasse.at>
2018-04-24 10:18:02 +0200

    Fixed model and texture issues and logic of BlockHerbgarden

--- src/main/java/com/korlimann/korlisfoodcraft/blocks/BlockHerbgarden.java
+++ src/main/java/com/korlimann/korlisfoodcraft/blocks/BlockHerbgarden.java
@@ -11,6 +11,7 @@ import net.minecraft.block.BlockTallGrass;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
@@ -30,7 +31,7 @@ public class BlockHerbgarden extends BlockBush implements IHasModel {
 	}
 	
 	@Override
-	public void onBlockDestroyedByPlayer(World worldIn, BlockPos pos, IBlockState state) {
+	public void onBlockHarvested(World worldIn, BlockPos pos, IBlockState state, EntityPlayer player) {
 		Random r = new Random();
 		int rand = r.nextInt(4)+1;
 		int r1 = 0;
@@ -38,37 +39,35 @@ public class BlockHerbgarden extends BlockBush implements IHasModel {
 		BlockPos posNew = pos.up();
 		if(rand == 4) {
 			for(int i = 0; i<4; i++) {
-				r1 = r.nextInt(10)+1;
-				if(r1 == 1) worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), posNew.getY(), pos.getZ(), new ItemStack(ModItems.CORN_SEED)));
-				if(r1 == 2) worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), posNew.getY(), pos.getZ(), new ItemStack(ModItems.EISBERGSALAT)));
-				if(r1 == 3) worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), posNew.getY(), pos.getZ(), new ItemStack(ModItems.HOPS)));
-				if(r1 == 4) worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), posNew.getY(), pos.getZ(), new ItemStack(ModItems.LEEK)));
-				if(r1 == 5) worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), posNew.getY(), pos.getZ(), new ItemStack(ModItems.MALT)));
-				if(r1 == 6) worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), posNew.getY(), pos.getZ(), new ItemStack(ModItems.OLIVE)));
-				if(r1 == 7) worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), posNew.getY(), pos.getZ(), new ItemStack(ModItems.OREGANO)));
-				if(r1 == 8) worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), posNew.getY(), pos.getZ(), new ItemStack(ModItems.PARSLEY)));
-				if(r1 == 9) worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), posNew.getY(), pos.getZ(), new ItemStack(ModItems.PEPPER)));
-				if(r1 == 10) worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), posNew.getY(), pos.getZ(), new ItemStack(ModItems.TOMATOSEEDS)));
+				r1 = r.nextInt(10)+1; 
+				if(r1 == 1) player.addItemStackToInventory(new ItemStack(ModItems.CORN_SEED));
+				if(r1 == 2) player.addItemStackToInventory(new ItemStack(ModItems.EISBERGSALAT));
+				if(r1 == 3) player.addItemStackToInventory(new ItemStack(ModItems.HOPS));
+				if(r1 == 4) player.addItemStackToInventory(new ItemStack(ModItems.LEEK));
+				if(r1 == 5) player.addItemStackToInventory(new ItemStack(ModItems.MALT));
+				if(r1 == 6) player.addItemStackToInventory(new ItemStack(ModItems.OLIVE));
+				if(r1 == 7) player.addItemStackToInventory(new ItemStack(ModItems.OREGANO));
+				if(r1 == 8) player.addItemStackToInventory(new ItemStack(ModItems.PARSLEY));
+				if(r1 == 9) player.addItemStackToInventory(new ItemStack(ModItems.PEPPER));
+				if(r1 == 10) player.addItemStackToInventory(new ItemStack(ModItems.TOMATOSEEDS));
 			}
 		}
 		else if(rand < 4) {
 			for(int i = 0; i<3; i++) {
 				r2 = r.nextInt(10)+1;
-				if(r2 == 1) worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), posNew.getY(), pos.getZ(), new ItemStack(ModItems.CORN_SEED)));
-				if(r2 == 2) worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), posNew.getY(), pos.getZ(), new ItemStack(ModItems.EISBERGSALAT)));
-				if(r2 == 3) worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), posNew.getY(), pos.getZ(), new ItemStack(ModItems.HOPS)));
-				if(r2 == 4) worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), posNew.getY(), pos.getZ(), new ItemStack(ModItems.LEEK)));
-				if(r2 == 5) worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), posNew.getY(), pos.getZ(), new ItemStack(ModItems.MALT)));
-				if(r2 == 6) worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), posNew.getY(), pos.getZ(), new ItemStack(ModItems.OLIVE)));
-				if(r2 == 7) worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), posNew.getY(), pos.getZ(), new ItemStack(ModItems.OREGANO)));
-				if(r2 == 8) worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), posNew.getY(), pos.getZ(), new ItemStack(ModItems.PARSLEY)));
-				if(r2 == 9) worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), posNew.getY(), pos.getZ(), new ItemStack(ModItems.PEPPER)));
-				if(r2 == 10) worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), posNew.getY(), pos.getZ(), new ItemStack(ModItems.TOMATOSEEDS)));
+				if(r2 == 1) player.addItemStackToInventory(new ItemStack(ModItems.CORN_SEED));
+				if(r2 == 2) player.addItemStackToInventory(new ItemStack(ModItems.EISBERGSALAT));
+				if(r2 == 3) player.addItemStackToInventory(new ItemStack(ModItems.HOPS));
+				if(r2 == 4) player.addItemStackToInventory(new ItemStack(ModItems.LEEK));
+				if(r2 == 5) player.addItemStackToInventory(new ItemStack(ModItems.MALT));
+				if(r2 == 6) player.addItemStackToInventory(new ItemStack(ModItems.OLIVE));
+				if(r2 == 7) player.addItemStackToInventory(new ItemStack(ModItems.OREGANO));
+				if(r2 == 8) player.addItemStackToInventory(new ItemStack(ModItems.PARSLEY));
+				if(r2 == 9) player.addItemStackToInventory(new ItemStack(ModItems.PEPPER));
+				if(r2 == 10) player.addItemStackToInventory(new ItemStack(ModItems.TOMATOSEEDS));
 			}
 		}
 	}
-	
-	
 
 	@Override
 	public void registerModels() {
