2c5e9e39100bd19d085b1a3f6c7c1a401a247801
Stefan Kormann <kor17541@spengergasse.at>
2018-05-29 20:39:39 +0200

    Added new Base class for Seed/Food

---
+++ src/main/java/com/korlimann/korlisfoodcraft/items/ItemBaseSeedFood.java
@@ -0,0 +1,30 @@
+package com.korlimann.korlisfoodcraft.items;
+
+import com.korlimann.korlisfoodcraft.Main;
+import com.korlimann.korlisfoodcraft.util.IHasModel;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemSeedFood;
+import net.minecraft.item.ItemSeeds;
+
+public class ItemBaseSeedFood extends ItemSeedFood implements IHasModel {
+
+	/*
+	 * This class creates custom seeds for plants, crops, etc. 
+	 * */
+	
+	public ItemBaseSeedFood(String name, Block crops, Block soil, int healAmount, float potionEffectProbability) {
+		super(healAmount, potionEffectProbability, crops, soil);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(Main.korlissushicraft);
+		
+		//ModItems.ITEMS.add(this);
+	}
+
+	@Override
+	public void registerModels() {
+		Main.proxy.registerItemRenderer(this, 0, "inventory");
+	}
+
+}
