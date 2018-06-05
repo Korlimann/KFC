e70ccceb59a1d748c8299a1db13fa007f68fb48f
theOnlyHorst <buschsteffmichi@chello.at>
2018-06-05 13:12:00 +0200

    Release Versionchange 0.2

--- src/main/java/com/korlimann/korlisfoodcraft/util/Reference.java
+++ src/main/java/com/korlimann/korlisfoodcraft/util/Reference.java
@@ -7,7 +7,7 @@ public class Reference {
 	 * */
 	public static final String MOD_ID = "kfc";
 	public static final String NAME = "Korli's FoodCraft";
-	public static final String VERSION = "v1.0";
+	public static final String VERSION = "v0.2";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "com.korlimann.korlisfoodcraft.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "com.korlimann.korlisfoodcraft.proxy.CommonProxy";
