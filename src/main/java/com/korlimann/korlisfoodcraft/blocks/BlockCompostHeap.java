aac67b01d51af02048168ee74f3c1837b47ec8bb
stefa <stefa@DESKTOP-L17P1KH>
2018-06-26 14:14:27 +0200

    Updated CompostHeap

--- src/main/java/com/korlimann/korlisfoodcraft/blocks/BlockCompostHeap.java
+++ src/main/java/com/korlimann/korlisfoodcraft/blocks/BlockCompostHeap.java
@@ -1,5 +1,7 @@
 package com.korlimann.korlisfoodcraft.blocks;
 
+import java.util.Random;
+
 import com.korlimann.korlisfoodcraft.Main;
 import com.korlimann.korlisfoodcraft.init.ModItems;
 import com.korlimann.korlisfoodcraft.util.IHasModel;
@@ -13,12 +15,15 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class BlockCompostHeap extends Block implements IHasModel {
 
 	public static final PropertyInteger FILL = PropertyInteger.create("fill", 0, 7);
+	public static final PropertyInteger AGE = PropertyInteger.create("age", 0, 3);
 	
 	public BlockCompostHeap(String name, boolean creativeTab) {
 		super(Material.WOOD);
@@ -28,7 +33,7 @@ public class BlockCompostHeap extends Block implements IHasModel {
 		setCreativeTab(Main.korlissushicraft);
 	}
 
-	@Override
+	/*@Override
 	public void onBlockClicked(World worldIn, BlockPos pos, EntityPlayer playerIn) {
 		ItemStack hhi = playerIn.getHeldItemMainhand();
 		IBlockState state = worldIn.getBlockState(pos);
@@ -43,7 +48,49 @@ public class BlockCompostHeap extends Block implements IHasModel {
 				playerIn.addItemStackToInventory(new ItemStack(ModItems.FERTILIZER));
 				worldIn.setBlockState(pos, state.withProperty(FILL, Integer.valueOf(0)), 2);
 		}
+	}*/
+	
+	@Override
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		ItemStack hhi = playerIn.getHeldItem(hand);
+			
+		if(!worldIn.isRemote) {
+			if(((Integer)state.getValue(FILL)).intValue()<7) {
+				if(hhi.getItem() != null) {
+					if(hhi.getItem() == ModItems.BANANA_PEEL) {
+						worldIn.setBlockState(pos, state.withProperty(FILL, Integer.valueOf(((Integer)state.getValue(FILL)).intValue() + 1)), 2);
+						return true;
+					}
+				} else {return false;}
+			} else if(((Integer)state.getValue(AGE)).intValue()==3) {
+					playerIn.addItemStackToInventory(new ItemStack(ModItems.FERTILIZER));
+					worldIn.setBlockState(pos, state.withProperty(FILL, Integer.valueOf(0)), 2);
+					worldIn.setBlockState(pos, state.withProperty(AGE, Integer.valueOf(0)), 2);
+					return true;
+			}
+			return false;
+		}
+		return false;
 	}
+	
+	@Override
+	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
+		if(!worldIn.isRemote) {
+			if(((Integer)state.getValue(FILL)).intValue()==7) {	
+				int i = ((Integer)state.getValue(AGE)).intValue();
+
+	            if (i < 3 && net.minecraftforge.common.ForgeHooks.onCropsGrowPre(worldIn, pos, state, rand.nextInt(5) == 0))
+	            {
+	                worldIn.setBlockState(pos, state.withProperty(AGE, Integer.valueOf(i + 1)), 2);
+	                net.minecraftforge.common.ForgeHooks.onCropsGrowPost(worldIn, pos, state, worldIn.getBlockState(pos));
+	            }
+			}
+		}
+	}
+	
+	/*public boolean fillHeap(World worldIn, BlockPos pos, IBlockState state, EntityPlayer player) {
+		
+	}*/
 	
 	 public IBlockState getStateFromMeta(int meta)
 	    {
